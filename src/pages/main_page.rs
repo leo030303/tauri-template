09147fa631bdfbd7ca2af8c02487e48f2892da0d
Leo Ring <leoring03@gmail.com>
2024-07-30 01:10:24 +0100

    Better command example on client side

--- src/pages/main_page.rs
+++ src/pages/main_page.rs
@@ -1,7 +1,19 @@
 use leptos::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct GreetArgs<'a> {
+    name: &'a str,
+}
 
 #[component]
 pub fn MainPage() -> impl IntoView {
+    let (name, set_name) = create_signal(String::new());
+    spawn_local(async move {
+        let args = to_value(&GreetArgs { name: &name.get() }).unwrap();
+        let new_msg = invoke("greet", args).await.as_string().unwrap();
+        set_greet_msg.set(new_msg);
+    });
     view! {
         <div>
             <div class="bg-body-secondary sticky-top">
